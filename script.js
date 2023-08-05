10b99db7a033036aa3d57265488ff86b8a15f266
rx-equity <139679935+rx-equity@users.noreply.github.com>
2023-08-05 22:45:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -43,10 +43,35 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.screenY > 100);
 
-    menuIcon.classList.toggle('bx-x')
-    naavbar.classList.toggle('active')
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
 
 
-}
+};
+ScrollReveal({
+     reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 200
+     });
 
+     ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+     
+     ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+
+     ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+     ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+
+
+     const typed = new Typed('.multiple-text', // <-- Add a closing parenthesis here
+     {
+         strings: ['Full-Stack Developer', 'Web Design', 'Blogger'],
+         typeSpeed: 100,
+         backSpeed: 100,
+         backDelay: 1000,
+         loop: true
+     });
+     
